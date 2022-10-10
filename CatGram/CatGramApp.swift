208e23e7db9cdb44bf648d393913e1953b854451
Iury Vasconcelos <iury0393@gmail.com>
2022-10-10 09:30:01 -0300

    Initial Commit

---
+++ CatGram/CatGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatGramApp.swift
+//  CatGram
+//
+//  Created by Iury Vasconcelos on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CatGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
