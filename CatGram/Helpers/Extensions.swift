132d54e1e6b855352a2ba00b3cb94584da4ab920
Iury Vasconcelos <iury0393@gmail.com>
2022-10-14 13:15:53 -0300

    Adding the tabbar

---
+++ CatGram/Helpers/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  CatGram
+//
+//  Created by Iury Vasconcelos on 14/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct MyTheme {
+        
+        static var purpleColor: Color {
+            return Color("ColorPurple")
+        }
+        static var pinkColor: Color {
+            return Color("ColorPink")
+        }
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+    }
+}
---
+++ CatGram/Views/Screens/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  CatGram
+//
+//  Created by Iury Vasconcelos on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            Text("Screen 2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen 3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            Text("Screen 4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
